0e047674dcf1b494a21e8da9f6dfebcbc89a879a
Javier Granados <hola@javiergranados.dev>
2022-04-29 20:50:55 +0200

    Implement fade in and fade out animations

--- src/screens/Animation101Screen.tsx
+++ src/screens/Animation101Screen.tsx
@@ -1,15 +1,58 @@
-import React from 'react';
-import { StyleSheet, View } from 'react-native';
+import React, { useRef, useState } from 'react';
+import { Animated, Button, StyleSheet, View } from 'react-native';
 
 export const Animation101Screen = () => {
+  const [visible, setVisible] = useState<boolean>(false);
+  const [disabled, setDisabled] = useState<boolean>(false);
+  const opacity = useRef(new Animated.Value(0)).current;
+
+  const fadeIn = () => {
+    Animated.timing(opacity, {
+      toValue: 1,
+      duration: 600,
+      useNativeDriver: true,
+    }).start(() => {
+      setDisabled(false);
+      setVisible(true);
+    });
+  };
+
+  const fadeOut = () => {
+    Animated.timing(opacity, {
+      toValue: 0,
+      duration: 600,
+      useNativeDriver: true,
+    }).start(() => {
+      setDisabled(false);
+      setVisible(false);
+    });
+  };
+
+  const handleClick = () => {
+    setDisabled(true);
+    if (visible) {
+      fadeOut();
+    } else {
+      fadeIn();
+    }
+  };
+
+  const title: string = disabled ? 'Animating...' : visible ? 'Fade Out' : 'Fade In';
+
   return (
-    <View style={{ flex: 1 }}>
-      <View style={styles.box} />
+    <View style={styles.container}>
+      <Animated.View style={{ ...styles.box, marginBottom: 20, opacity: opacity }} />
+      <Button title={title} onPress={handleClick} disabled={disabled} />
     </View>
   );
 };
 
 const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
   box: {
     backgroundColor: '#5856D6',
     width: 150,
