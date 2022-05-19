63e76d2af0254e93d2a9999af6290766d3c9e884
Javier Granados <hola@javiergranados.dev>
2022-05-19 13:01:19 +0200

    Implement some text inputs

--- src/screens/TextInputScreen.tsx
+++ src/screens/TextInputScreen.tsx
@@ -1,13 +1,66 @@
-import React from 'react';
-import { StyleSheet, TextInput, View } from 'react-native';
+import React, { useState } from 'react';
+import {
+  Keyboard,
+  KeyboardAvoidingView,
+  Platform,
+  ScrollView,
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+  TouchableWithoutFeedback,
+} from 'react-native';
 import HeaderTitle from '../components/HeaderTitle';
 
 export const TextInputScreen = () => {
+  const [form, setForm] = useState({
+    name: '',
+    email: '',
+    phone: '',
+  });
+
+  const onChange = (value: string, field: string) =>
+    setForm({
+      ...form,
+      [field]: value,
+    });
+
   return (
-    <View style={styles.container}>
-      <HeaderTitle title="TextInputs" />
-      <TextInput style={styles.textInput} />
-    </View>
+    <KeyboardAvoidingView behavior={Platform.OS === 'ios' ? 'padding' : 'height'}>
+      <ScrollView>
+        <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+          <View style={styles.container}>
+            <HeaderTitle title="TextInputs" />
+            <TextInput
+              style={styles.textInput}
+              placeholder="Nombre"
+              autoCorrect={false}
+              autoCapitalize="words"
+              onChangeText={(value: string) => onChange(value, 'name')}
+            />
+            <TextInput
+              style={styles.textInput}
+              placeholder="Email"
+              autoCorrect={false}
+              autoCapitalize="none"
+              onChangeText={(value: string) => onChange(value, 'email')}
+              keyboardType="email-address"
+            />
+            <Text style={styles.formContainer}>{JSON.stringify(form, null, 5)}</Text>
+            <Text style={styles.formContainer}>{JSON.stringify(form, null, 5)}</Text>
+            <TextInput
+              style={styles.textInput}
+              placeholder="Teléfono"
+              onChangeText={(value: string) => onChange(value, 'phone')}
+              keyboardType="phone-pad"
+              keyboardAppearance="dark"
+            />
+            <Text style={styles.formContainer}>{JSON.stringify(form, null, 5)}</Text>
+            <View style={{ height: 100 }} />
+          </View>
+        </TouchableWithoutFeedback>
+      </ScrollView>
+    </KeyboardAvoidingView>
   );
 };
 
@@ -21,5 +74,10 @@ const styles = StyleSheet.create({
     height: 50,
     paddingHorizontal: 10,
     borderRadius: 10,
+    marginVertical: 10,
+  },
+  formContainer: {
+    fontSize: 25,
+    marginTop: 20,
   },
 });
