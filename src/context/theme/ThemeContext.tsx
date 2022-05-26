612fc91724f8b735167beb3209e9b5f663fc7d83
Javier Granados <hola@javiergranados.dev>
2022-05-26 11:53:07 +0200

    Set the initial theme based on the system preferences

--- src/context/theme/ThemeContext.tsx
+++ src/context/theme/ThemeContext.tsx
@@ -1,19 +1,28 @@
-import React, { createContext, useReducer } from 'react';
+import React, { createContext, useEffect, useReducer } from 'react';
 import { ThemeState, ThemeContextProps, LIGHT_THEME, DARK_THEME } from './ThemeTypes';
 import { themeReducer } from './ThemeReducer';
-
-const themeInitialState: ThemeState = {
-  theme: LIGHT_THEME,
-};
+import { useColorScheme } from 'react-native';
 
 export const ThemeContext = createContext({} as ThemeContextProps);
 
 export const ThemeProvider = ({ children }: any) => {
+  const colorScheme = useColorScheme();
+  const themeInitialState: ThemeState = {
+    theme: colorScheme === 'dark' ? DARK_THEME : LIGHT_THEME,
+  };
+
   const [state, dispatch] = useReducer(themeReducer, themeInitialState);
 
   const setLightTheme = () => dispatch({ type: 'SET_LIGHT_THEME', payload: LIGHT_THEME });
 
   const setDarkTheme = () => dispatch({ type: 'SET_DARK_THEME', payload: DARK_THEME });
+
+  useEffect(() => {
+    if ((colorScheme === 'dark' && state.theme.dark) || (colorScheme !== 'dark' && !state.theme.dark)) {
+      return;
+    }
+    colorScheme === 'dark' ? setDarkTheme() : setLightTheme();
+  }, [colorScheme]);
 
   return (
     <ThemeContext.Provider
